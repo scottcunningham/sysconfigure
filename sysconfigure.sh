7007e7a3289c10793fcf36f3a49be05e93d9bf71
Scott Cunningham <scottcunningham92@gmail.com>
2012-12-02 19:16:01 +0000

    Fixed small mistake, forgot to remove a 'quit' while testing

--- sysconfigure.sh
+++ sysconfigure.sh
@@ -3,12 +3,12 @@
 # Scott Cunningham <cunninsc@tcd.ie>
 # http://bitbucket.com/scottbpc/sysconfigure
 
+echo_colour haha
+
 SYSCONFIGURE_PATH=`pwd`
 DATETIME=`date | sed s/' '/-/g | sed s/:/_/g`
 
 echo $DATETIME
-
-exit
 
 # Should always start this script in $HOME
 cd 

b86b9e347d87a02c1bc78c05707e17a06349748f
Scott Cunningham <scottcunningham92@gmail.com>
2012-12-02 18:59:15 +0000

    First commit (this is such a boring commit message)

---
+++ sysconfigure.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Scott Cunningham <cunninsc@tcd.ie>
+# http://bitbucket.com/scottbpc/sysconfigure
+
+SYSCONFIGURE_PATH=`pwd`
+DATETIME=`date | sed s/' '/-/g`
+
+# Should always start this script in $HOME
+cd 
+
+# Install packages
+if [ -f $SYSCONFIGURE_PATH/pkgs ]
+    echo Installing `cat $SYSCONFIGURE_PATH/pkgs`
+    then sudo apt-get update && sudo apt-get install `cat $SYSCONFIGURE_PATH/pkgs`
+else
+    echo "File ´pkgs´ missing in folder $SYSCONFIGUREPATH"
+    exit 1
+fi
+
+# Set up git
+echo 
+echo Setting up git
+sleep 1
+git config --global user.name "Scott Cunningham"
+git config --global user.email "cunninsc@tcd.ie"
+
+# Get shell config
+echo "Fetching zsh config from bitbucket"
+mv .zshrc .zshrc.$DATETIME.old
+git clone https://scottbpc@bitbucket.org/scottbpc/.zshrc.git
+
+# Change shell - this won´t work until we fully log out/in though 
+echo "Changing shell to zsh"
+chsh -s `which zsh`
+
+# Get vim config
+echo "Backing up old vim config in format .vim{,rc}.\$DATE.old"
+if [ -f .vimrc ] 
+then
+    echo "Backing up vimrc"
+    mv .vimrc .vimrc.$DATETIME.old
+fi
+if [ -d .vim -o -f .vim ] 
+then
+    echo "Backing up .vim"
+    mv .vim .vim.$DATETIME.old
+fi
+
+echo "Grabbing vim config/plugins from bitbucket"
+git clone https://scottbpc@bitbucket.org/scottbpc/.vim.git
+cd .vim 
+
+ln -s .vim/.vimrc ~/
+git submodule init
+git submodule update --recursive
+
+# update vim plugins 
+git pull origin master
+git submodule foreach git pull origin master
+
+echo "Installing google-chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome-$DATETIME.deb
+sudo dpkg -i chrome-$DATETIME.deb
+
+# Fix any broken dependencies that Chrome leaves behind
+sudo apt-get -f install
+
+rm chrome-$DATETIME.deb
+
+# Go $HOME
+echo "Done!"
+cd
